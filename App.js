2397fcc6f207a3e90ea5638cd224238192c4fb07
Juan Isaza <jisazamp@gmail.com>
2020-10-08 17:51:58 -0500

    Stack Navigator

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const HomeScreen = ({navigation}) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to details screen"
+        onPress={() => navigation.navigate("Details")}
+        />
     </View>
+  );
+};
+
+const DetailsScreen = ({navigation}) => {
+  return (
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to details screen... again"
+        onPress={() => navigation.push("Details")}
+        />
+        <Button
+        title="Go to home"
+        onPress={() => navigation.navigate("Home")}
+        />
+        <Button
+        title="Go back"
+        onPress={() => navigation.goBack()}
+        />
+        <Button
+        title="Go to the first screen"
+        onPress={() => navigation.popToTop()}
+        />
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: '#009387',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold'
+        }
+      }}>
+        <Stack.Screen name="Home" component={HomeScreen} options={{
+          title: 'Overview'
+        }} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
